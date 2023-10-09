e532e39e982decd63361bf2dccfed0224623abfc
Mansi Shinde <shindemansi2804@gmail.com>
2023-10-09 11:17:15 +0530

    Commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../Styles/Login.css";
-import data from "../datas.json"
 
 const Login = () => {
   const [email, setEmail] = useState("");
