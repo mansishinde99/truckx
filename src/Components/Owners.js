8acbcef83edfe22512087119cc8bc3de73811da0
Mansi Shinde <shindemansi2804@gmail.com>
2023-10-12 20:30:47 +0530

    Commit

--- src/Components/Owners.js
+++ src/Components/Owners.js
@@ -10,7 +10,7 @@ const Owners = ({ slides }) => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1, // Display one slide at a time
+    slidesToShow: 1, 
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
