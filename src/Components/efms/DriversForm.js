50dcb7a4a1c9bca97535673dad4bc9c569efdac0
Mansi Shinde <shindemansi2804@gmail.com>
2024-02-12 19:21:27 +0530

    Changes in driver form

--- src/Components/efms/DriversForm.js
+++ src/Components/efms/DriversForm.js
@@ -3,49 +3,40 @@ import "../../Styles/efmsStyle/DriversForm.css";
 
 const DriversForm = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [submittedData, setSubmittedData] = useState();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [cell, setCell] = useState("");
+  const [license, setLicense] = useState("");
+  const [info, setInfo] = useState([]);
+  const [submittedData, setSubmittedData] = useState(false);
 
   const openForm = (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     setIsOpen(true);
-    document.body.classList.add("blur");
   };
 
   const closeForm = (e) => {
     e.preventDefault();
     setIsOpen(false);
-    document.body.classList.remove("blur");
   };
 
   const submitForm = (e) => {
     e.preventDefault();
-    const firstName = e.target.elements.firstName.value;
-    const lastName = e.target.elements.lastName.value;
-    const email = e.target.elements.email.value;
-    const cell = e.target.elements.cell.value;
-    const license = e.target.elements.license.value;
+    const newData = { firstName, lastName, email, cell, license };
+    setInfo((nData) => [...nData, newData]);
+    setSubmittedData(true);
 
-    console.log("First Name:", firstName);
-    console.log("Last Name:", lastName);
-    console.log("Email Id:", email);
-    console.log("Cell:", cell);
-    console.log("License:", license);
-
-    e.target.elements.firstName.value = "";
-    e.target.elements.lastName.value = "";
-    e.target.elements.email.value = "";
-    e.target.elements.cell.value = "";
-    e.target.elements.license.value = "";
-
-    const newData = {firstName,lastName,email,cell,license};
-
-    setSubmittedData(...submittedData, newData);
-
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setCell("");
+    setLicense("");
   };
 
   return (
     <div className="popup">
-      <button className="add-drivers" onClick={(e) => openForm(e)}>
+      <button className="add-drivers" onClick={openForm}>
         + Add Drivers
       </button>
 
@@ -57,33 +48,65 @@ const DriversForm = () => {
             <div className="content">
               <div className="filling">
                 <p>First Name</p>
-                <input placeholder="First Name" name="firstName" required />
+                <input
+                  placeholder="First Name"
+                  name="firstName"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                />
               </div>
               <div className="filling">
                 <p>Last Name</p>
-                <input placeholder="Last Name" name="lastName" required />
+                <input
+                  placeholder="Last Name"
+                  name="lastName"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                />
               </div>
             </div>
             <div className="content">
               <div className="filling">
                 <p>Email Id</p>
-                <input placeholder="Email Id" name="email" type="email" required/>
+                <input
+                  placeholder="Email Id"
+                  name="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </div>
               <div className="filling">
                 <p>Cell</p>
-                <input placeholder="Phone Number" name="cell" type="number" required/>
+                <input
+                  placeholder="Phone Number"
+                  name="cell"
+                  type="number"
+                  value={cell}
+                  onChange={(e) => setCell(e.target.value)}
+                  required
+                />
               </div>
             </div>
             <div className="content">
               <div className="filling">
                 <p>License</p>
-                <input placeholder="License No." name="license" required />
+                <input
+                  placeholder="License No."
+                  name="license"
+                  value={license}
+                  onChange={(e) => setLicense(e.target.value)}
+                  required
+                />
               </div>
             </div>
             <button type="submit" className="submit-button">
               Submit
             </button>
-            <button className="cancel-button" onClick={(e) => closeForm(e)}>
+            <button className="cancel-button" onClick={closeForm}>
               Cancel
             </button>
           </form>
@@ -93,11 +116,30 @@ const DriversForm = () => {
       {submittedData && (
         <div className="submitted-data">
           <h1>Submitted Data:</h1>
-          <p>First Name: {submittedData.firstName}</p>
-          <p>Last Name: {submittedData.lastName}</p>
-          <p>Email Id: {submittedData.email}</p>
-          <p>Cell: {submittedData.cell}</p>
-          <p>License: {submittedData.license}</p>
+          {info.map((val, index) => (
+            <div className="data" key={index}>
+              <p>
+                <strong>First Name:</strong> {val.firstName}
+              </p>
+              <p>
+                <strong>Last Name:</strong> {val.lastName}
+              </p>
+              <p>
+                <strong>Email Id:</strong> {val.email}
+              </p>
+              <p>
+                <strong>Cell:</strong> {val.cell}
+              </p>
+              <p>
+                <strong>License:</strong> {val.license}
+              </p>
+
+              <div className="blue-buttons">
+                <button className="edit-button">Edit</button>
+                <button className="delete-button">Delete</button>
+              </div>
+            </div>
+          ))}
         </div>
       )}
     </div>
